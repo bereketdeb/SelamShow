33e7d026b2487ab48a74836dfdce4a9f05a3b396
bereketdeb <bereketdeb@gmail.com>
2017-04-26 17:02:27 -0500

    Initial push for our spring boot project

---
+++ src/main/java/com/selam/SelamApplication.java
@@ -0,0 +1,12 @@
+package com.selam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelamApplication.class, args);
+	}
+}
