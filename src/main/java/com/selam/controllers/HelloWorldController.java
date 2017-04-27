e6cf2b0ba13fc39d51dfc0b6b97b495cefdb3925
bereketdeb <bereketdeb@gmail.com>
2017-04-27 00:01:55 -0500

    additional commit

---
+++ src/main/java/com/selam/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.selam.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/")
+	public String sayHello(){
+		
+		return "index";
+	}
+}
